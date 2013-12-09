71de6e10d78e8284ed5a1bba0e804fb9a1486ec3
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-12-09 16:51:28 +0900

    Use capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Capybara::DSL
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
