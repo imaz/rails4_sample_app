d44aa730c47ffb4a217986bb473bf3673ae4b469
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-12-10 15:15:54 +0900

    Change to simply routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  # この書き方の方がRailsの慣例に従っているらしい（不安感ある）
+  match '/home', to: 'static_pages#home', via: 'get'
+
+  get :help,    to: 'static_pages#help'
+  get :about,   to: 'static_pages#about'
+  get :contact, to: 'static_pages#contact'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,48 +4,48 @@ describe "Static Pages" do
 
   describe "Home page" do
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
     end
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to have_content('Sample App')
     end
   end
 
   describe "Help page" do
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
     end
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
   end
 
   describe "About page" do
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
     end
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
   end
 
   describe "Contact page" do
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
   end
